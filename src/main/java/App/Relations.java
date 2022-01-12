db88b09ee004b9753b639bb5cd9cb0a605e91ee8
AndrasTarlos <76171862+AndrasTarlos@users.noreply.github.com>
2022-01-12 15:16:34 +0100

    Added enum

---
+++ src/main/java/App/Relations.java
@@ -0,0 +1,33 @@
+package App;
+
+public enum Relations {
+    BinaryTreeSort_InvTeilsort1000, BinaryTreeSort_InvTeilsort10000, BinaryTreeSort_InvTeilsort100000,
+    BinaryTreeSort_Random1000, BinaryTreeSort_Random10000, BinaryTreeSort_Random100000,
+    BinaryTreeSort_Teilsort1000, BinaryTreeSort_Teilsort10000, BinaryTreeSort_Teilsort100000,
+
+    BubbleSort_InvTeilsort1000, BubbleSort_InvTeilsort10000, BubbleSort_InvTeilsort100000,
+    BubbleSort_Random1000, BubbleSort_Random10000, BubbleSort_Random100000,
+    BubbleSort_Teilsort1000, BubbleSort_Teilsort10000, BubbleSort_Teilsort100000,
+
+    HeapSort_InvTeilsort1000, HeapSort_InvTeilsort10000, HeapSort_InvTeilsort100000,
+    HeapSort_Random1000, HeapSort_Random10000, HeapSort_Random100000,
+    HeapSort_Teilsort1000, HeapSort_Teilsort10000, HeapSort_Teilsort100000,
+
+    InsertionSort_InvTeilsort1000, InsertionSort_InvTeilsort10000, InsertionSort_InvTeilsort100000,
+    InsertionSort_Random1000, InsertionSort_Random10000, InsertionSort_Random100000,
+    InsertionSort_Teilsort1000, InsertionSort_Teilsort10000, InsertionSort_Teilsort100000,
+
+    Mergesort_InvTeilsort1000, Mergesort_InvTeilsort10000, Mergesort_InvTeilsort100000,
+    Mergesort_Random1000, Mergesort_Random10000, Mergesort_Random100000,
+    Mergesort_Teilsort1000, Mergesort_Teilsort10000, Mergesort_Teilsort100000,
+
+    QuickSort_InvTeilsort1000, QuickSort_InvTeilsort10000, QuickSort_InvTeilsort100000,
+    QuickSort_Random1000, QuickSort_Random10000, QuickSort_Random100000,
+    QuickSort__Teilsort1000, QuickSort_Teilsort10000, QuickSort_Teilsort100000,
+
+    ShakerSort_InvTeilsort1000, ShakerSort_InvTeilsort10000, ShakerSort_InvTeilsort100000,
+    ShakerSort_Random1000, ShakerSort_Random10000, ShakerSort_Random100000,
+    ShakerSort_Teilsort1000, ShakerSort_Teilsort10000, ShakerSort_Teilsort100000,
+
+    BinaryTreeSort, BubbleSort, HeapSort, InsertionSort, Mergesort, QuickSort, ShakerSort
+}

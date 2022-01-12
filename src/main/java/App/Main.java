ab68b8f5b673e37439aa7baefcc27dcd5b4bb43b
AndrasTarlos <76171862+AndrasTarlos@users.noreply.github.com>
2022-01-12 15:15:07 +0100

    Modified Main.java

--- src/main/java/App/Main.java
+++ src/main/java/App/Main.java
@@ -26,7 +26,6 @@ public class Main {
         fileArrays = reader.getFileArrays();
 
         sortingTypes = new SortingInterface[7];
-
         sortingTypes[0] = new BinaryTreeSort();
         sortingTypes[1] = new HeapSort();
         sortingTypes[2] = new InsertionSort();
@@ -36,22 +35,22 @@ public class Main {
         sortingTypes[6] = new BubbleSort();
 
         updateSortingResults();
-        /*
         for (long[] res: results) {
-            for (int i = 0; i < 4; i++) {
+            for (int i = 0; i < 5; i++) {
                 System.out.print(res[i] + ", ");
             }
             System.out.println();
         }
-         */
     }
 
     public void updateSortingResults() {
         results = new Vector<>();
+        int i = 0;
         for (SortingInterface s: sortingTypes) {
             for (Vector<Integer> v: fileArrays) {
                 s.sort(v);
-                results.add(new long[] {s.getAmountOfComparisons(), s.getStorageSpaceRequired(), s.getTimeForSorting(), s.getWriteChanges()});
+                // i steht für algorithmus und k für die Files
+                results.add(new long[] {i++, s.getAmountOfComparisons(), s.getStorageSpaceRequired(), s.getTimeForSorting(), s.getWriteChanges()});
             }
         }
     }

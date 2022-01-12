f064251111f70df05f13f1638c617501a881e5ba
AndrasTarlos <76171862+AndrasTarlos@users.noreply.github.com>
2022-01-12 15:14:47 +0100

    Added Relations.enum and modified Main.java

--- src/main/java/App/GUI.java
+++ src/main/java/App/GUI.java
@@ -1,11 +1,5 @@
 package App;
 
-import javax.swing.*;
-import java.awt.*;
-import java.awt.image.ImageObserver;
-import java.awt.image.ImageProducer;
-import java.util.Objects;
-
 /**
  *
  *
@@ -13,30 +7,8 @@ import java.util.Objects;
  * @since 2022.01.12
  * @version 0.0.1
  */
-public class GUI {
-
-    public GUI(String args) {
-        super(new ImageIcon(Objects.requireNonNull(Main.class.getResource("../../resources/images/sort.png"))));
-        tray = SystemTray.getSystemTray();
-    }
-
-    public void startGui() {
-
-    }
-
-    private void addIfTrayIsSupported() {
-        if (SystemTray.isSupported()) {
-            try {
-                SystemTray.getSystemTray().add(this);
-            } catch (AWTException e) {
-                e.printStackTrace();
-            }
-        } else {
-            runWithoutTray();
-        }
-    }
-
-    private void runWithoutTray() {
+public class GUI{
+    public GUI() {
 
     }
 }
--- src/main/java/App/JFX_GUI.java
+++ src/main/java/App/JFX_GUI.java
@@ -1,34 +1,144 @@
 package App;
 
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
+import javafx.application.Platform;
+import javafx.geometry.Pos;
+import javafx.scene.Node;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
-public class newFile extends Application {
-    public static void main(String[] args) {
-        launch(args);
-    }
+import javax.imageio.ImageIO;
+import java.io.IOException;
+import java.net.URL;
+import java.util.Objects;
+
+public class JFX_GUI extends Application {
+    private static final String iconLoc = Objects.requireNonNull(Main.class.getResource("../../resources/images/sort.png")).getPath();
+
+    // application stage is stored so that it can be shown and hidden based on system tray icon operations.
+    private Stage stage;
+
+    // format used to display the current time in a tray icon notification.
+    //private DateFormat timeFormat = SimpleDateFormat.getTimeInstance();
 
     @Override
-    public void start(Stage primaryStage) {
-        primaryStage.setTitle("Hello World!");
-        Button btn = new Button();
-        btn.setText("Say 'Hello World'");
-        btn.setOnAction(new EventHandler<ActionEvent>() {
+    public void start(Stage stage) {
+        // stores a reference to the stage.
+        this.stage = stage;
 
-            @Override
-            public void handle(ActionEvent event) {
-                System.out.println("Hello World!");
+        // instructs the javafx system not to exit implicitly when the last application window is shut.
+        Platform.setImplicitExit(false);
+
+        // sets up the tray icon (using awt code run on the swing thread).
+        javax.swing.SwingUtilities.invokeLater(this::addAppToTray);
+
+        // out stage will be translucent, so give it a transparent style.
+        stage.initStyle(StageStyle.TRANSPARENT);
+
+        // create the layout for the javafx stage.
+        StackPane layout = new StackPane(createContent());
+        layout.setStyle("-fx-background-color: rgba(255, 255, 255, 0.5);");
+        layout.setPrefSize(300, 200);
+
+        // this dummy app just hides itself when the app screen is clicked.
+        // a real app might have some interactive UI and a separate icon which hides the app window.
+        layout.setOnMouseClicked(event -> stage.hide());
+
+        // a scene with a transparent fill is necessary to implement the translucent app window.
+        Scene scene = new Scene(layout);
+        scene.setFill(Color.TRANSPARENT);
+
+        stage.setScene(scene);
+    }
+
+    /**
+     * For this dummy app, the (JavaFX scenegraph) content, just says "hello, world".
+     * A real app, might load an FXML or something like that.
+     *
+     * @return the main window application content.
+     */
+    private Node createContent() {
+        Label hello = new Label("hello, world");
+        hello.setStyle("-fx-font-size: 40px; -fx-text-fill: forestgreen;");
+        Label instructions = new Label("(click to hide)");
+        instructions.setStyle("-fx-font-size: 12px; -fx-text-fill: orange;");
+
+        VBox content = new VBox(10, hello, instructions);
+        content.setAlignment(Pos.CENTER);
+
+        return content;
+    }
+
+    /**
+     * Sets up a system tray icon for the application.
+     */
+    private void addAppToTray() {
+        try {
+            // ensure awt toolkit is initialized.
+            java.awt.Toolkit.getDefaultToolkit();
+
+            // app requires system tray support, just exit if there is no support.
+            if (!java.awt.SystemTray.isSupported()) {
+                System.out.println("No system tray support, application exiting.");
+                Platform.exit();
             }
-        });
 
-        StackPane root = new StackPane();
-        root.getChildren().add(btn);
-        primaryStage.setScene(new Scene(root, 300, 250));
-        primaryStage.show();
+            // set up a system tray icon.
+            java.awt.SystemTray tray = java.awt.SystemTray.getSystemTray();
+            URL imageLoc = new URL(iconLoc);
+            java.awt.Image image = ImageIO.read(imageLoc);
+            java.awt.TrayIcon trayIcon = new java.awt.TrayIcon(image);
+
+            // if the user double-clicks on the tray icon, show the main app stage.
+            trayIcon.addActionListener(event -> Platform.runLater(this::showStage));
+
+            // if the user selects the default menu item (which includes the app name),
+            // show the main app stage.
+            java.awt.MenuItem openItem = new java.awt.MenuItem("hello, world");
+            openItem.addActionListener(event -> Platform.runLater(this::showStage));
+
+            // the convention for tray icons seems to be to set the default icon for opening
+            // the application stage in a bold font.
+            java.awt.Font defaultFont = java.awt.Font.decode(null);
+            java.awt.Font boldFont = defaultFont.deriveFont(java.awt.Font.BOLD);
+            openItem.setFont(boldFont);
+
+            // to really exit the application, the user must go to the system tray icon
+            // and select the exit option, this will shutdown JavaFX and remove the
+            // tray icon (removing the tray icon will also shut down AWT).
+            java.awt.MenuItem exitItem = new java.awt.MenuItem("Exit");
+            exitItem.addActionListener(event -> {
+                Platform.exit();
+                tray.remove(trayIcon);
+            });
+
+            // setup the popup menu for the application.
+            final java.awt.PopupMenu popup = new java.awt.PopupMenu();
+            popup.add(openItem);
+            popup.addSeparator();
+            popup.add(exitItem);
+            trayIcon.setPopupMenu(popup);
+
+            // add the application tray icon to the system tray.
+            tray.add(trayIcon);
+        } catch (java.awt.AWTException | IOException e) {
+            System.out.println("Unable to init system tray");
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Shows the application stage and ensures that it is brought ot the front of all stages.
+     */
+    private void showStage() {
+        if (stage != null) {
+            stage.show();
+            stage.toFront();
+        }
     }
 }

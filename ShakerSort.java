65a3136ed25ad20245ad194ef818047a4ca77427
SvenW3686 <76094202+SvenW3686@users.noreply.github.com>
2022-01-12 13:59:50 +0100

    Add files via upload

---
+++ ShakerSort.java
@@ -0,0 +1,46 @@
+public class Shakersort {
+
+    public int[] intArr = { 16, 23, 14, 7, 21, 20, 6, 1, 17, 13, 12, 9, 3, 19 };
+    int k;
+
+    public int[] sort() {
+
+        int i = 0, l = intArr.length;
+        while (i < l) {
+            shaker1(i, l);
+            l--;
+            shaker2(i, l);
+            i++;
+        }
+        return intArr;
+    }
+
+    private void shaker1(int i, int l) {
+        for (int j = i; j < l - 1; j++) {
+            if (intArr[j] > intArr[j + 1]) {
+                k = intArr[j];
+                intArr[j] = intArr[j + 1];
+                intArr[j + 1] = k;
+            }
+        }
+    }
+
+    private void shaker2(int i, int l) {
+        for (int j = l - 1; j >= i; j--) {
+            if (intArr[j] > intArr[j + 1]) {
+                k = intArr[j];
+                intArr[j] = intArr[j + 1];
+                intArr[j + 1] = k;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Shakersort ss = new Shakersort();
+        int[] arr = ss.sort();
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(i + 1 + ": " + arr[i]);
+        }
+    }
+
+}

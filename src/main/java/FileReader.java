d433f54548522d93384e14a29db810122dbf9666
AndrasTarlos <76171862+AndrasTarlos@users.noreply.github.com>
2022-01-12 11:47:58 +0100

    changed FileReader.java

--- src/main/java/FileReader.java
+++ src/main/java/FileReader.java
@@ -1,3 +1,36 @@
-public class FileReader {
+import java.io.File;
+import java.io.IOException;
+import java.util.Vector;
 
+public class FileReader {
+    private final Vector<File> files;
+    public FileReader() {
+
+        files = new Vector<>();
+        try {
+            addFilesToList();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        for (File file: files) {
+            System.out.println(file.getName());
+        }
+    }
+
+    public Vector<Integer[]> getList() {
+
+        return
+    }
+
+    public void addFilesToList() throws IOException{
+        File folder = new File(new File("./src/main/resources/Files/").getCanonicalPath());
+        File[] listOfFiles = folder.listFiles();
+
+        assert listOfFiles != null;
+        for (File file : listOfFiles) {
+            if (file.isFile()) {
+                files.add(file);
+            }
+        }
+    }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
+        new Main().startApp();
+    }
+
+    public void startApp() {
+        FileReader reader = new FileReader();
 
     }
 }

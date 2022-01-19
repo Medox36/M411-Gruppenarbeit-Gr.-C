15e55d50ccf96d7e605128c0571d65f0bd57957d
Medox36 <giuntinil@bzz.ch>
2022-01-19 10:38:04 +0100

    added DataArraySyntaxException.java

---
+++ src/main/java/App/DataArraySyntaxException.java
@@ -0,0 +1,14 @@
+package App;
+
+/**
+ *
+ *
+ * @author Lorenzo Giuntini (Medox36)
+ * @since 2022.01.19
+ * @version 0.0.1
+ */
+public class DataArraySyntaxException extends Exception{
+    public DataArraySyntaxException() {
+        super("The Vector<long[]> is inconsistent and was found not to contain the data in the right order.");
+    }
+}

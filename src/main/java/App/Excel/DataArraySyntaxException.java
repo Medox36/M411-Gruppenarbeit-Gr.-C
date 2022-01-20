38f94daec74b56292210d291754cb36346108323
Medox36 <giuntinil@bzz.ch>
2022-01-20 22:17:19 +0100

    removed unnecessary empty lines after closing the class

--- src/main/java/App/Excel/DataArraySyntaxException.java
+++ src/main/java/App/Excel/DataArraySyntaxException.java
@@ -7,7 +7,7 @@ package App.Excel;
  * @since 2022.01.19
  * @version 0.0.1
  */
-public class DataArraySyntaxException extends Exception{
+public class DataArraySyntaxException extends Exception {
     public DataArraySyntaxException() {
         super("The Vector<long[]> is inconsistent and was found not to contain the data in the right order.");
     }

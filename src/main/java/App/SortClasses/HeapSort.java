b167e80850822f288f0a3b30716c474b97258bd7
Medox36 <giuntinil@bzz.ch>
2022-01-22 22:35:41 +0100

    completed HeapSort.java

--- src/main/java/App/SortClasses/HeapSort.java
+++ src/main/java/App/SortClasses/HeapSort.java
@@ -5,6 +5,12 @@ import App.SortingInterface;
 
 import java.util.Vector;
 
+/**
+ *
+ * @author Lorenzo Giuntini (Medox36)
+ * @since 2022.01.22
+ * @version 0.1.1
+ */
 public class HeapSort extends ArrayCopier implements SortingInterface {
     private long writeChanges = 0;
     private long timeForSorting = 0;
@@ -20,7 +26,42 @@ public class HeapSort extends ArrayCopier implements SortingInterface {
     }
 
     public void sort(int[] arr) {
+        int n = arr.length;
+        for (int i = n / 2 - 1; i >= 0; i--)
+            heapify(arr, n, i);
 
+        for (int i = n - 1; i > 0; i--) {
+            int temp = arr[0];
+            arr[0] = arr[i];
+            arr[i] = temp;
+            writeChanges += 2;
+
+            heapify(arr, i, 0);
+        }
+    }
+
+    void heapify(int[] arr, int n, int i) {
+        int largest = i;
+        int l = 2 * i + 1;
+        int r = 2 * i + 2;
+
+        if (l < n && arr[l] > arr[largest])
+            largest = l;
+        amountOfComparisons += 2;
+
+        if (r < n && arr[r] > arr[largest])
+            largest = r;
+        amountOfComparisons += 2;
+
+        if (largest != i) {
+            amountOfComparisons++;
+            int swap = arr[i];
+            arr[i] = arr[largest];
+            arr[largest] = swap;
+            writeChanges += 2;
+
+            heapify(arr, n, largest);
+        }
     }
 
     @Override

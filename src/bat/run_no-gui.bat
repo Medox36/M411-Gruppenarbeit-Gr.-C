2acfa727be53774265caba4908c6ed7c07c937fc
Medox36 <giuntinil@bzz.ch>
2022-01-23 21:30:58 +0100

    changed max memory heap in .bat files

--- src/bat/run_no-gui.bat
+++ src/bat/run_no-gui.bat
@@ -1,3 +1,3 @@
 @echo off
 title Sorting Analytics
-java -Xmx1G -Djdk.attach.allowAttachSelf -jar M411-Gruppenarbeit-Gr.-C.jar --no-gui
+java -Xmx512m -Djdk.attach.allowAttachSelf -jar M411-Gruppenarbeit-Gr.-C.jar --no-gui

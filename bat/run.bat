1d3042bd40a028d51a30266a5380c5e0bac4f877
Medox36 <giuntinil@bzz.ch>
2022-01-25 10:52:42 +0100

    made so the .bat files open in a specific sized command prompt window

--- bat/run.bat
+++ bat/run.bat
@@ -1,3 +1,4 @@
 @echo off
 title Sorting Analytics
+mode con: cols=115 lines=25
 java -Xmx512m -Djdk.attach.allowAttachSelf -jar M411-Gruppenarbeit-Gr.-C.jar
--- bat/run_no-gui.bat
+++ bat/run_no-gui.bat
@@ -1,3 +1,4 @@
 @echo off
 title Sorting Analytics - no gui
+mode con: cols=115 lines=25
 java -Xmx512m -Djdk.attach.allowAttachSelf -jar M411-Gruppenarbeit-Gr.-C.jar --no-gui

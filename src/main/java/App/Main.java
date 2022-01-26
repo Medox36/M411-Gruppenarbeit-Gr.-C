8a5856be2b4749763f47d42dd5bee7f1693d9c68
AndrasTarlos <tarlosa@bzz.ch>
2022-01-26 10:13:00 +0100

    Added some comments in Main.java

--- src/main/java/App/Main.java
+++ src/main/java/App/Main.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 
 /**
  * <h1>Main</h1>
+ * <p>This is the class where the program begins. It can handle a couple <br>of arguments. The program can be started with or without GUI.</p>
  *
  * @author Andras Tarlos
  * @author Lorenzo Giuntini (Medox36)
@@ -19,7 +20,9 @@ public class Main {
     public void startApp(String[] args) {
         if (args.length != 0) {
             if (args[0].equals("--help") || args[0].equals("-h")) {
+                System.out.println("--------------------------------------");
                 System.out.println("--no-gui\t- There will be no GUI shown");
+                System.out.println("--------------------------------------");
             } else if (args[0].equals("--no-gui")) {
                 System.out.println("--no-gui: No GUI will be shown");
                 new Sorting().start();

8705985ed5e2eb29cf1549b63d3e95ec12e10e4e
Medox36 <giuntinil@bzz.ch>
2022-01-26 01:12:17 +0100

    modified module-info.java

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,11 +1,11 @@
 module App {
     requires javafx.fxml;
     requires javafx.controls;
+    requires javafx.media;
     requires java.desktop;
     requires org.apache.poi.poi;
     requires org.apache.poi.ooxml;
     requires jol.core;
-    requires javafx.media;
     opens App to javafx.graphics;
     exports App;
     exports App.SortClasses;

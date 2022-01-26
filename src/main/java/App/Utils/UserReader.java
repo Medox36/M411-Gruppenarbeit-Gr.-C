7297296ff3e7bc2954b9ad1a3387cfd5b29a38d5
Medox36 <giuntinil@bzz.ch>
2022-01-26 09:33:43 +0100

    commented UserReader.java

--- src/main/java/App/Utils/UserReader.java
+++ src/main/java/App/Utils/UserReader.java
@@ -3,20 +3,40 @@ package App.Utils;
 import java.util.Scanner;
 
 /**
+ * <h1>UserReader</h1>
+ * <h3>Used for reading inputs from the command line.</h3>
+ * It uses the java.util.Scanner to read a char input from the command line and evaluates it.
+ *
+ * @see java.util.Scanner
+ *
  * @author Lorenzo Giuntini (Medox36)
  * @since 2022.01.24
  * @version 0.1.1
  */
 public class UserReader {
 
-    // attributes
+    /**
+     * reference to the Scanner
+     */
     static Scanner sc;
 
-    // constructor
+    /**
+     * Constructor creating a new Scanner with param System.in
+     */
     public UserReader() {
         sc = new Scanner(System.in);
     }
 
+    /**
+     * This method will ask the User a question wich can be answered with yes or no.<br>
+     * The User will be asked as many times as needed until the User types in either 'y' (Yes) or 'n' (No).<br><br>
+     *
+     * @param text Information wich will be given to the User to know what about he needs to decide.
+     * @return boolean value if either the user answered the question with yes or no.
+     *
+     * @apiNote
+     * The case of the given input will be dismissed. So UpperChase and LowerChase will be accepted.
+     */
     public boolean readBooleanJN(String text) {
         if (!text.equals("")) {
             System.out.println(text);
